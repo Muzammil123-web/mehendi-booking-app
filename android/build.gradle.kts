f668ece6776ba81afca1a1872a74ebd41c56c830
Muzammil123-web <muzammilshaik70@gmail.com>
2026-07-27 13:27:36 +0530

    Build Fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,21 +4,25 @@ allprojects {
         mavenCentral()
     }
 }
-
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
-
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    afterEvaluate {
+        extensions.findByName("android")?.let { ext ->
+            if (ext is com.android.build.gradle.BaseExtension) {
+                ext.compileSdkVersion(36)
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
 }
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
